afb627442853c58f8d4dce4feb15a517ee4f5986
jenalit <j.lituranjan@gmail.com>
2026-01-09 05:13:18 +0530

    Spring Boot internationalization first commit

--- src/main/java/com/learnwithranjan/restservices/hello/HelloWorldController.java
+++ src/main/java/com/learnwithranjan/restservices/hello/HelloWorldController.java
@@ -1,12 +1,19 @@
 package com.learnwithranjan.restservices.hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private ResourceBundleMessageSource bundleMessageSource;
 	
 	//@RequestMapping(method = RequestMethod.GET,path="/hello")
 	@GetMapping("/helloo")
@@ -18,5 +25,24 @@ public class HelloWorldController {
 	public UserDetails getUserDaeils() {
 		return new UserDetails("litu", "jena", "bangalore");
 	}
+
+	
+	@GetMapping("/hello-int")
+	public String getMessageINI18NFormat(
+	    @RequestHeader(name = "Accept-Language", required = false) String acceptLanguage) {
+
+	    Locale locale = (acceptLanguage == null || acceptLanguage.isBlank())
+	        ? Locale.getDefault()
+	        : Locale.forLanguageTag(acceptLanguage.split(",")[0].trim());
+
+	    return bundleMessageSource.getMessage("label.hello", null, locale);
+	}
+	
+	
+	@GetMapping("/hello-int2")
+	public String getMessageINI18NFormat2() {
+	    return bundleMessageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
+	}
+
 
 }
